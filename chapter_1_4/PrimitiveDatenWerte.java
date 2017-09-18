5e9ec7df656244e826b9f1db1f4ed5a67d943ffd
PascalKu <kutpasc@gmail.com>
2017-09-18 21:50:59 +0200

    Added tasks 4 and 5 from chapter 1.4

---
+++ chapter_1_4/PrimitiveDatenWerte.java
@@ -0,0 +1,22 @@
+package chapter_1_4;
+
+/**
+ * Beschreiben Sie hier die Klasse PrimitiveDatenWerte.
+ * 
+ * @author (Ihr Name) 
+ * @version (eine Versionsnummer oder ein Datum)
+ */
+public class PrimitiveDatenWerte {
+    
+    public PrimitiveDatenWerte() {
+        System.out.println("Byte: [" + Byte.MIN_VALUE + " bis " + Byte.MAX_VALUE + "]");
+        System.out.println("Short: [" + Short.MIN_VALUE + " bis " + Short.MAX_VALUE + "]");
+        
+        System.out.println("Integer: [" + Integer.MIN_VALUE + " bis " + Integer.MAX_VALUE + "]");
+        System.out.println("Double: [" + Double.MIN_VALUE + " bis " + Double.MAX_VALUE + "]");
+        
+        System.out.println("Long: [" + Long.MIN_VALUE + " bis " + Long.MAX_VALUE + "]");
+        System.out.println("Float: [" + Float.MIN_VALUE + " bis " + Float.MAX_VALUE + "]");
+        
+    }
+}
