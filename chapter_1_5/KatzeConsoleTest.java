6571bb6eddc83165cee52632599598b750fb40e4
Pascal <kutpasc@gmail.com>
2017-09-19 09:16:05 +0200

    Create KatzeConsoleTest.java

---
+++ chapter_1_5/KatzeConsoleTest.java
@@ -0,0 +1,31 @@
+package chapter_1_5;
+
+import libs.Console;
+/**
+ * Write a description of class KatzeConsoleTest here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class KatzeConsoleTest extends Console {
+    
+    public KatzeConsoleTest() {
+        String name, farbe, rasse;
+        Integer alter;
+        
+        System.out.print("Name: ");
+        name = readString();
+        
+        System.out.print("Farbe: ");
+        farbe = readString();
+        
+        System.out.print("Rasse: ");
+        rasse = readString();
+        
+        System.out.print("Alter: ");
+        alter = readInteger();
+        
+        Katze katze = new Katze(name, farbe.toLowerCase(), rasse, alter);
+        katze.sayName();
+    }
+}
