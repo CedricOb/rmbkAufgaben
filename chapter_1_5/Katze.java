addead7efa0b1394bec5803ad1bc42c53d8c21bf
Pascal <kutpasc@gmail.com>
2017-09-19 09:20:14 +0200

    Update Katze.java

--- chapter_1_5/Katze.java
+++ chapter_1_5/Katze.java
@@ -11,6 +11,9 @@ public class Katze {
     private String name = null, farbe = null, rasse = null;
     private Integer alter = 0;
     
+    /**
+     * Inital method to set all variables of the cat
+     */
     public Katze(String name, String farbe, String rasse, Integer alter) {
         this.name = name;
         this.farbe = farbe + "e";
@@ -19,14 +22,23 @@ public class Katze {
         this.alter = alter;
     }
     
+    /**
+     * change the name of the cat
+     */
     public void setName(String name) {
         this.name = name;
     }
     
+    /**
+     * update the skin color of the cat
+     */
     public void setFarbe(String farbe) {
         this.farbe = farbe;
     }
     
+    /**
+     * print all data of the cat
+     */
     public void sayName() {
         String string = String.format("Hey, ich heiße %s und bin eine %s Katze. Ich bin %s und %s Jahre jung.", name, farbe, rasse, alter);
         System.out.println(string);
