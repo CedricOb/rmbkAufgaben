50e7a38284c9e278cfaf2c646fde87e101b36799
Pascal <kutpasc@gmail.com>
2017-09-19 09:11:16 +0200

    Create KatzeTest.java

---
+++ chapter_1_5/KatzeTest.java
@@ -0,0 +1,16 @@
+package chapter_1_5;
+
+import chapter_1_5.Katze;
+/**
+ * Write a description of class KatzeTest here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class KatzeTest {
+    
+    public KatzeTest() {
+        Katze katze = new Katze("Max", "rot", "American Curl", 2);
+        katze.sayName();
+    }
+}
