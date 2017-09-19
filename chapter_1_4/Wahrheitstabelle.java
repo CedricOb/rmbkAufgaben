360cc695ddb09608dcb3a880f4682f7af0d72f83
Pascal <kutpasc@gmail.com>
2017-09-19 08:25:00 +0200

    Create Wahrheitstabelle.java

---
+++ chapter_1_4/Wahrheitstabelle.java
@@ -0,0 +1,18 @@
+package chapter_1_4;
+
+import java.util.*;
+/**
+ * Write a description of class Wahrheitstabelle here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Wahrheitstabelle {
+    
+    public Wahrheitstabelle() {
+        System.out.println("00 -> " + (false && false));
+        System.out.println("01 -> " + (false && true));
+        System.out.println("10 -> " + (true && false));
+        System.out.println("11 -> " + (true && true));
+    }
+}
